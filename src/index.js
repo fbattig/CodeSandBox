bb3c2e55553f532fe9541d6fc91982567a0ea9a5
Felix <fbattig@yahoo.com>
2018-03-24 19:22:13 -0400

    video_list_item

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
-const API_KEY = 'AIzaSyAwmDlW8puXpMvfRRrH-5Cg-5V9gB9dWZd';
+import VideoList from './video_list';
+const API_KEY = 'AIzaSyAwmDlW8puXpMvfRRrH-5Cg-5V9gB9dWZc';
 
 const styles = {
   fontFamily: 'sans-serif',
@@ -15,16 +16,14 @@ class App extends Component {
 
     this.state = { videos: [] };
     
-    YTSearch
-    ( { key: API_KEY, term: 'surfboards' }, 
-      (videos) => {this.setstate({ videos });}
-    );
+    YTSearch ( { key: API_KEY, term: 'spirit' }, (videos) => {this.setState({ videos });}    );
   }
 
   render() {
     return (
       <div style={styles} >
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div >
     );
   }
