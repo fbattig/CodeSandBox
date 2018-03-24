f12cde2b8b37fdd362be12fbb36ce34e2727538e
Felix <fbattig@yahoo.com>
2018-03-24 10:15:39 -0400

    enhanced index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './search_bar';
+const API_KEY = 'AIzaSyAwmDlW8puXpMvfRRrH-5Cg-5V9gB9dWZd';
 
 const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
 };
 
-const App = () => (
-  <div style={styles}>
-    <SearchBar />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props); 
+
+    this.state = { videos: [] };
+    
+    YTSearch
+    ( { key: API_KEY, term: 'surfboards' }, 
+      (videos) => {this.setstate({ videos });}
+    );
+  }
+
+  render() {
+    return (
+      <div style={styles} >
+        <SearchBar />
+      </div >
+    );
+  }
+}
 
 render(<App />, document.getElementById('root'));
