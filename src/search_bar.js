2c1b13cd7a3c5f6fa4d7d5bcd7e3eaeb62273992
Felix <fbattig@yahoo.com>
2018-03-24 08:19:47 -0400

    Controlled Component

--- src/search_bar.js
+++ src/search_bar.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
 
 
   render() {
-    return(
+    return (
+
       <div>
-       <input onChange={event => console.log(event.target.value)} />
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+
       </div>
     )
   }
