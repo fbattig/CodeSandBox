7d0d701f627eec60000f3f58bab6f77664611e78
Felix <fbattig@yahoo.com>
2018-03-25 11:59:20 -0400

    Search for videos

--- src/index.js
+++ src/index.js
@@ -18,7 +18,11 @@ class App extends Component {
 
     };
 
-    YTSearch({ key: API_KEY, term: 'react' }, (videos) => {
+    this.videoSearch('god');
+  }
+
+  videoSearch(term){
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({
         videos,
         selectedVideo: videos[0]
@@ -30,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div >
-        <SearchBar />
+        <SearchBar  onSearchTermChange={ term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
